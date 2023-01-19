1be5928e089f41512ecd45f98bd3500122039d55
Muhammad Muhajir <muhajirframe@gmail.com>
2023-01-19 21:13:17 +0700

    add tawk.to

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import e from "cors";
 import { type NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
+import Script from "next/script";
 import { useState } from "react";
 import { trpc } from "../utils/trpc";
 
@@ -16,6 +17,22 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center">
         <Auth />
       </main>
+      <Script
+        id="live-chat"
+        dangerouslySetInnerHTML={{
+          __html: `
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/63c94c66c2f1ac1e202e8c34/1gn554snp';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+      `,
+        }}
+      ></Script>
     </>
   );
 };
